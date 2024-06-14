43b65f664ac80b8a0ee4706b02054261a1371004
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-14 14:45:19 +0000

    chore: Update compute_product function to use recursion for computing the product

--- python/exercise1/compute_product.py
+++ python/exercise1/compute_product.py
@@ -1,10 +1,10 @@
 def compute_product(m, n):
-    return 0
+    return 0 if n == 0 else m + compute_product(m, n - 1)
 
 
 def main():
-    m = 5
-    n = 3
+    m = 4
+    n = 2
     result = compute_product(m, n)
     print(result)
 
