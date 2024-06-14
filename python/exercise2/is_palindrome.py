6cc11a1cc1fffda64383b98621bf4cfee67562a0
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-14 14:57:18 +0000

    Add test cases for determining palindromes

---
+++ python/exercise2/is_palindrome.py
@@ -0,0 +1,11 @@
+def is_palindrome(s):
+    return False
+
+
+def main():
+    s = input("Provide string entries to be checked: ").strip()
+    print(is_palindrome(s))
+
+
+if __name__ == "__main__":
+    main()
