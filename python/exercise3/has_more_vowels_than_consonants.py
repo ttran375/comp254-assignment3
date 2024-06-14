ce2ee2fbf798285c797e9205057cedfd42b5674e
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-14 15:23:03 +0000

    Update has_more_vowels_than_consonants function to use recursion for determining strings having more vowels than consonants

--- python/exercise3/has_more_vowels_than_consonants.py
+++ python/exercise3/has_more_vowels_than_consonants.py
@@ -1,5 +1,22 @@
+def is_vowel(char):
+    return char.lower() in "aeiou"
+
+
+def count_vowels(s):
+    return 0 if not s else (1 if is_vowel(s[0]) else 0) + count_vowels(s[1:])
+
+
+def count_consonants(s):
+    return (
+        0
+        if not s
+        else (1 if s[0].isalpha() and not is_vowel(s[0]) else 0)
+        + count_consonants(s[1:])
+    )
+
+
 def has_more_vowels_than_consonants(s):
-    return False
+    return count_vowels(s) > count_consonants(s)
 
 
 def main():
