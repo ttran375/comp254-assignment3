22cbbb048f1b6207451a449135241afd887201aa
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-14 15:14:25 +0000

    Update count_vowels function to use recursion for counting vowels

--- python/exercise2/count_vowels.py
+++ python/exercise2/count_vowels.py
@@ -1,5 +1,9 @@
+def is_vowel(char):
+    return char.lower() in "aeiou"
+
+
 def count_vowels(s):
-    return 0
+    return 0 if not s else (1 if is_vowel(s[0]) else 0) + count_vowels(s[1:])
 
 
 def main():
