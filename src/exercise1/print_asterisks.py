fdd4f130f06087b09ec3f692fe26765dc31dcc0f
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-26 12:59:57 +0000

    chore: Update print_asterisks

--- src/exercise1/print_asterisks.py
+++ src/exercise1/print_asterisks.py
@@ -1,19 +1,17 @@
 def print_asterisks(n, current=1):
+    """
+    Prints a pattern of asterisks using recursion.
+    """
     return (
         None
+        # If the current line number exceeds n, terminate the recursion
         if current > n
         else (
+            # Print asterisks for the current line
             print("*" * current)
+            # Recursive call to print the next line
             or print_asterisks(n, current + 1)
+            # Print the current line again when returning from the recursion
             or (current != n and print("*" * current))
         )
     )
-
-
-def main():
-    max_stars = int(input("Enter the number of asterisks of the maximum line: "))
-    print_asterisks(max_stars)
-
-
-if __name__ == "__main__":
-    main()
